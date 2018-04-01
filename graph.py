c04adf7f4d4527b4ee8a77df83cf86301eefabc7
Pollycarpus <fijarmayoza.rabbi@gmail.com>
2018-04-01 14:18:45 +0700

    kelarin kuy

--- graph.py
+++ graph.py
@@ -54,29 +54,54 @@ def euclid(now, dest):
 def cost(g, h):
     return g + h
 
-#Test
-nf = input("Masukkan file matriks ketetanggaan: ")    
-print(getMatriks(nf))
-lok = input("Masukkan file koordinat node:")
-print(getLokasi(lok))
-pos=nx.get_node_attributes(getGraph(getMatriks(nf), getLokasi(lok)),'pos')
-nx.draw(getGraph(getMatriks(nf), getLokasi(lok)), pos, with_labels=True, font_weight='bold')
-plt.show()
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    
+    while not frontier.empty():
+        current = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+    
+    return came_from, cost_so_far
 
-"""
-G = nx.Graph()
-G.add_node(1, pos=(0,0))
-G.add_node(2, pos=(1,5))
-G.add_node(3, pos=(2,5))
-G.add_node(4, pos=(2,7))
-G.add_node(5, pos=(3,5))
+if __name__ == '__main__':
+    #Test
+    nf = input("Masukkan file matriks ketetanggaan: ")    
+    print(getMatriks(nf))
+    lok = input("Masukkan file koordinat node:")
+    print(getLokasi(lok))
+    pos=nx.get_node_attributes(getGraph(getMatriks(nf), getLokasi(lok)),'pos')
+    nx.draw(getGraph(getMatriks(nf), getLokasi(lok)), pos, with_labels=True, font_weight='bold')
+    plt.show()
 
-G.add_edge(1,2)
-G.add_edge(2,3)
-G.add_edge(1,3)
-G.add_edge(4,5)
-G.add_edge(3,5)
-pos=nx.get_node_attributes(G,'pos')
-nx.draw(G, pos, with_labels=True, font_weight='bold')
-plt.show()
-"""
+    """
+    G = nx.Graph()
+    G.add_node(1, pos=(0,0))
+    G.add_node(2, pos=(1,5))
+    G.add_node(3, pos=(2,5))
+    G.add_node(4, pos=(2,7))
+    G.add_node(5, pos=(3,5))
+
+    G.add_edge(1,2)
+    G.add_edge(2,3)
+    G.add_edge(1,3)
+    G.add_edge(4,5)
+    G.add_edge(3,5)
+    pos=nx.get_node_attributes(G,'pos')
+    nx.draw(G, pos, with_labels=True, font_weight='bold')
+    plt.show()
+    """
